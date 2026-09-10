9654a0cad9210487c6beca789b3fe1fb20def35e
Sandy <sandilyamachiraju33@gmail.com>
2026-09-10 15:54:56 +0530

    feat: initialize core directory and file paths for Git repository structure

--- main.py
+++ main.py
@@ -7,6 +7,19 @@ class Repository:
         self.path = Path(path).resolve()
         self.git_dir = self.path / ".git"
 
+        # .git/objects
+        self.objects_dir = self.git_dir / "objects"
+
+        # .git/refs
+        self.ref_dir = self.git_dir / "refs"
+        self.heads_dir = self.ref_dir / "heads"
+
+        # HEAD file
+        self.head_file = self.git_dir / "HEAD"
+
+        # .git/index
+        self.index_file = self.git_dir / "index"
+
 def main():
     parser = argparse.ArgumentParser(
         description="Git from Scratch",
